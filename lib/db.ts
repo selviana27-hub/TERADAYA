2eaeb1f1da67c2725429ec0b8fc0cdc1821c471e
Gelora Selviana <geloraselviana02@example.com>
2026-06-16 21:33:59 +0700

    Connect Railway Database

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'teradaya',
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
